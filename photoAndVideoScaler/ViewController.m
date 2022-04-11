dcb6e4804c93a20fa8282dbdedd194d032a63ee7
BingBingJia <jdb156158@sina.com>
2022-04-11 14:42:16 +0800

    添加新的刻度尺

--- photoAndVideoScaler/ViewController.m
+++ photoAndVideoScaler/ViewController.m
@@ -8,6 +8,7 @@
 #import "ViewController.h"
 #import "JDZoomView.h"
 #import "BottomFrameZoomView.h"
+#import "TimeRuler.h"
 
 // 拿到xib中的view
 #define XIBBundleView(bundle, xibName, _owner) [[bundle loadNibNamed:xibName owner:_owner options:nil] firstObject]
@@ -25,8 +26,8 @@
     
     //self.view.backgroundColor = [UIColor brownColor];
     
-    self.bottomFramesZoomView.imageCount = 8;
-    [self.view addSubview:self.bottomFramesZoomView];
+//    self.bottomFramesZoomView.imageCount = 8;
+//    [self.view addSubview:self.bottomFramesZoomView];
 
 //    JDZoomView *zooomView = [[JDZoomView alloc] initWithFrame:CGRectMake(0, 150, [UIScreen mainScreen].bounds.size.width, 600) duration:200];
 //    zooomView.backgroundColor = [UIColor lightGrayColor];
@@ -39,24 +40,33 @@
 //    subView.backgroundColor = [UIColor yellowColor];
 //    [zooomView.contentView addSubview:subView];
 //    self.subView = subView;
-}
-
-#pragma mark - JDZoomViewDelegate
-
-- (void)zoomView:(JDZoomView *)zoomView didChangePosition:(CGFloat)position currentTime:(NSTimeInterval)time{
     
-}
-- (void)zoomViewDidzoom:(JDZoomView *)zoomView atScale:(CGFloat)scale{
-    NSLog(@"scale = %f",scale);
-    self.subView.frame = CGRectMake(50 * scale, 50, 100 * scale, 30);
+    TimeRuler *time = [[TimeRuler alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height-250, self.view.bounds.size.width, 250)];
+    NSArray *array = @[@{@"start": @60,@"end": @300},
+                       @{@"start": @500,@"end": @800},
+                       @{@"start": @3600,@"end": @4800},
+                       @{@"start": @8501,@"end": @10000},
+                       @{@"start": @12000,@"end": @12312}];
+    [time setSelectedRange:array];
+    [self.view addSubview:time];
 }
 
-//创建header
-- (BottomFrameZoomView *)bottomFramesZoomView {
-    if (!_bottomFramesZoomView) {
-        _bottomFramesZoomView = XIBBundleView([NSBundle bundleForClass:[self class]], @"BottomFrameZoomView", nil);
-        _bottomFramesZoomView.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height-250, [UIScreen mainScreen].bounds.size.width, 250);
-    }
-    return _bottomFramesZoomView;
-}
+//#pragma mark - JDZoomViewDelegate
+//
+//- (void)zoomView:(JDZoomView *)zoomView didChangePosition:(CGFloat)position currentTime:(NSTimeInterval)time{
+//
+//}
+//- (void)zoomViewDidzoom:(JDZoomView *)zoomView atScale:(CGFloat)scale{
+//    NSLog(@"scale = %f",scale);
+//    self.subView.frame = CGRectMake(50 * scale, 50, 100 * scale, 30);
+//}
+//
+////创建header
+//- (BottomFrameZoomView *)bottomFramesZoomView {
+//    if (!_bottomFramesZoomView) {
+//        _bottomFramesZoomView = XIBBundleView([NSBundle bundleForClass:[self class]], @"BottomFrameZoomView", nil);
+//        _bottomFramesZoomView.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height-250, [UIScreen mainScreen].bounds.size.width, 250);
+//    }
+//    return _bottomFramesZoomView;
+//}
 @end
